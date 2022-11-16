65e20e374ed87c07df7c70b62738b3f3fbe4eeb2
Franklin Fontalvo <franklin.fontalvo.76@gmail.com>
2022-11-16 09:29:16 -0400

    comentado el template

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 const express = require('express');
 const cors = require('cors');
+
+// Importamos todaslas rutas registradas
 const routerApi = require('./routes');
 
+// Middlewares de error
 const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler');
 
 const app = express();
 const port = 3000;
 
+//middleware para que todas las peticiones se parseen a JSON
 app.use(express.json());
 
+//Lista de urls permitidas para solucionar el problema de cors headers
 const whitelist = ['http://localhost:8080', 'https://myapp.co'];
+
+//configuracion del cors
 const options = {
   origin: (origin, callback) => {
     if (whitelist.includes(origin)) {
@@ -19,19 +26,23 @@ const options = {
     }
   }
 }
+
+
 app.use(cors(options));
 
 app.get('/', (req, res) => {
   res.send('Hola mi server en express');
 });
 
-app.get('/nueva-ruta', (req, res) => {
-  res.send('Hola, soy una nueva ruta');
-});
 
+// app.get('/nueva-ruta', (req, res) => {
+//   res.send('Hola, soy una nueva ruta');
+// });
+
+//Se invocan todas las rutas declaradas
 routerApi(app);
 
-
+//algunos otros middlewares generales
 app.use(logErrors);
 app.use(boomErrorHandler);
 app.use(errorHandler);
--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,28 @@
 const express = require('express');
 
-const productsRouter = require('./products.router');
-const categoriesRouter = require('./categories.router');
-const usersRouter = require('./users.router');
+const teamsRouter = require('./teams.router');
+
+// const productsRouter = require('./products.router');
+// const categoriesRouter = require('./categories.router');
+// const usersRouter = require('./users.router');
 
 function routerApi(app) {
   const router = express.Router();
-  app.use('/api/v1', router);
-  router.use('/products', productsRouter);
-  router.use('/categories', categoriesRouter);
-  router.use('/users', usersRouter);
+
+  app.use('/api/', router);
+
+  // Registramos la ruta principal para conectar con la derivadas de esta.
+  // En este caso, las derivadas están en teamsRouter.
+  // Busque el archivo teamsRouter.js
+  app.use('/api/teams', teamsRouter);
+
+
+  // router.use('/products', productsRouter);
+  // router.use('/categories', categoriesRouter);
+  // router.use('/users', usersRouter);
+
+
+
 }
 
 module.exports = routerApi;
