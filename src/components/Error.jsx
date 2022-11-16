8854339a10b8c2671dd1d747fc68ec648d7397c6
Lgalvarini98 <galvariniluciano@gmail.com>
2022-11-16 19:21:55 -0300

    componente errores

---
+++ src/components/Error.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const Error = ({ error }) => {
+  toast.error(error, {
+    position: "top-right",
+    autoClose: 2000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "dark",
+  });
+
+  return (
+    <ToastContainer
+      position="top-right"
+      autoClose={2000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss={false}
+      draggable
+      pauseOnHover
+      theme="dark"
+    />
+  );
+};
+
+export default Error;
