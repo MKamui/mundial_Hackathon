d4eaf4cfabddae6929c2cc788673ab625abe82fe
ValentinaBonfanti <bonfantivalentina30@gmail.com>
2022-11-18 14:48:37 -0300

    equipos

--- client/src/components/Teams/Teams.jsx
+++ client/src/components/Teams/Teams.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import { getTeams } from "../../redux/actions";
 import { useSelector, useDispatch } from "react-redux";
-import Card from "./Card";
+import CardT from "./Card";
 import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
+import PaginationT from "./Pagination";
 
 export default function Teams(){
     const dispatch=useDispatch()
-    const allTeams=useSelector((state)=>state.teams)
+    const allTeams=useSelector((state)=>state.teams);
+
+    const [page, setPage] = useState(1);
+    const [forPage] = useState(8);//cant de pokemons q quiero poner por pag.
+    const [input, setInput] = useState(1)
+    const max = Math.ceil(allTeams.length / forPage); //4
     console.log(allTeams)
     
     
@@ -18,11 +24,13 @@ export default function Teams(){
     return(
         <div className="teams">
             {
-                allTeams?.map((e)=>{
+                allTeams && allTeams
+                .slice((page - 1) * forPage, (page - 1) * forPage + forPage)
+                .map((e)=>{
                     console.log(e.fifa_code)
                     return(
                         <fragment>
-                                <Card
+                                <CardT
                                     flag={e.flag}
                                     name={e.name}
                                     codigo={e.fifa_code}
@@ -35,6 +43,12 @@ export default function Teams(){
 
 
             }
+            <PaginationT
+          page={page}
+          setPage={setPage}
+          teamsPerPage={max}
+          input={input}
+          setInput={setInput}/>
         </div>
     )
 }
